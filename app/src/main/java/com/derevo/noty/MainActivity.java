dd23ba1db22932ca12f28a5da9b5924a7f84e5c3
Rodrigo Soriano <rodrigosoriano49@gmail.com>
2020-06-04 23:59:04 -0400

    push test

--- app/src/main/java/com/derevo/noty/MainActivity.java
+++ app/src/main/java/com/derevo/noty/MainActivity.java
@@ -13,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //push test
+
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setIcon(R.mipmap.ic_launcher);
     }
